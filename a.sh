7a66a0b9be0a28e7dab835da0344f02b12f27453
QuishCoder <danielderitaabdelahstudy@gmail.com>
2023-05-27 00:32:50 +0200

    0.1

---
+++ a.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "¡Hola desde Termux!"
+echo "Este es un script de ejemplo en Bash."
+
+# Puedes agregar más comandos aquí
+# Por ejemplo:
+echo "La fecha y hora actual es: $(date)"
+
+echo "El script ha finalizado."
